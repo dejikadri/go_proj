426fad7ec305da78a8fc352414f3ee4b4d675dce
Deji Kadri <dejikadri@gmail.com>
2023-03-29 13:23:00 -0400

    datatypes and prinf

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,12 +2,26 @@ package main
 import "fmt"
 
 func main(){
-	var conferenceName = "GopherCon"
-	const conferenceTickets = 60
-	var remainingTickets = 60
+	
 
-	fmt.Printf("Welcome to our %v conference booking app\n", conferenceName)
-	fmt.Printf("We have a total of %v tickets and are still %v available", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	var userName string
+	var numTickets int
+	userName ="Deji"
+	numTickets = 15
+
+
+	fmt.Printf("User %v bought %v tickets\n", userName, numTickets)
+	fmt.Printf("the data type of username is %T\n", userName)
+	fmt.Printf("the data type of numTickets is %T\n", numTickets)
+
+
+	// var conferenceName = "GopherCon"
+	// const conferenceTickets = 60
+	// var remainingTickets = 60
+
+	// fmt.Printf("Welcome to our %v conference booking app\n", conferenceName)
+	// fmt.Printf("We have a total of %v tickets and are still %v available\n", conferenceTickets, remainingTickets)
+	// fmt.Println("Get your tickets here to attend")
+
 }
 
